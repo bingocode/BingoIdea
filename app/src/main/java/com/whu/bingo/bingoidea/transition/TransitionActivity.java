a30d8acf95b918e99b0c94fb3ae4b63584cd5a48
zengbin <2416940975@qq.com>
2019-12-06 20:23:49 +0800

    feat: 修改kotlin 配置

---
+++ app/src/main/java/com/whu/bingo/bingoidea/transition/TransitionActivity.java
@@ -0,0 +1,14 @@
+package com.whu.bingo.bingoidea.transition;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.whu.bingo.bingoidea.R;
+
+public class TransitionActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_transition);
+  }
+}
